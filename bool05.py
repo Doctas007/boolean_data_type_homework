8351ccd7af3ccf23c40dd4c1eebb487de151fa8f
Erkinov Sardor <you@example.com>
2025-07-02 10:08:08 +0500

    uyga vazzfa qilindi

--- bool05.py
+++ bool05.py
@@ -1,10 +1,11 @@
 def main(a):
     """
-    check the following statement "The variable "a" is an odd number"
+   check the following statement "The variable "b" is positive"
     Args:
         a: int
     Returns:
         bool
     """
     # Write your code here
-    return 
+    return a>0
+print(main(8))
